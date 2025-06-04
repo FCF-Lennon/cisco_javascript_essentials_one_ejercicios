c0d59972a89761f788242bbb73798587cb75128f
pipedev <fcf.remote@gmail.com>
2025-06-04 14:01:21 -0400

    docs(examen_final): módulo 1 - curso JavaScript Essentials One (Cisco) - finalizado

---
+++ Modulo_01/modulo_01_examen.js
@@ -0,0 +1,165 @@
+// Pregunta 1 - ¿Dónde definitivamente no podremos ejecutar código JavaScript?
+
+/* 
+    Opciones:
+
+    Directamente en el procesador. / respuesta
+    En un entorno de servidor utilizando Node.js.
+    En un navegador web.
+    En un dispositivo móvil.
+*/
+
+// Pregunta 2 - El lenguaje TypeScript es:
+
+/* 
+    Opciones:
+
+    - Un nuevo lenguaje basado en JS que, entre otras cosas, introduce tipado estático. / respuesta
+    - Una variante de JS que se compila en lugar de interpretarse.
+    - El nombre original de JS, que ha cambiado con el tiempo.
+    - Un nombre alternativo para JS.
+*/
+
+// Pregunta 3 - El conjunto de herramientas básico necesario para desarrollar eficazmente código 
+// JS consta de dos elementos:
+
+/*
+    Opciones:
+
+    Intérprete, depurador.
+    Intérprete, gestor de paquetes.
+    Editor de código, intérprete.
+    Editor de código, depurador. / respuesta
+*/
+
+// Pregunta 4 - ¿Cuál no es tarea del intérprete?
+
+/*
+    Opciones:
+
+    - Verificar la corrección del código fuente del programa (sintaxis).
+    - Crear un entorno de ejecución para el programa.
+    - Transformar todo el código del programa en código de destino antes de la ejecución. / respuesta
+    - Transformar los comandos individuales del código fuente en el formato de destino.
+*/
+
+// Pregunta 5 - Usando JS, queremos mostrar la palabra "test" en la consola. 
+// ¿Qué sentencia usamos para lograrlo?
+
+/*
+    Opciones:
+
+    console("test");
+    console.log("test"); / respuesta
+    log("test");
+    console(log, "test");
+*/
+
+// Pregunta 6 - Un programa JS del lado del cliente:
+
+/*
+    Opciones:
+
+    - Requiere soporte del servidor para ejecutarse.
+    - Debe estar incrustado dentro de un documento HTML. / para cisco esta es la respuesta
+    - Puede integrarse dentro de un documento HTML o ejecutarse 
+      de forma independiente en el navegador. / para mi esta es la respuesta
+    - Se ejecuta directamente en un navegador web y puede contener 
+      un documento HTML en su interior.
+*/
+
+// Pregunta 7 - HTML es:
+
+/*
+    Opciones:
+
+    - Un lenguaje de programación para soportar cálculos matemáticos.
+    - Un protocolo de red.
+    - Uno de los formatos de compresión de datos.
+    - Un lenguaje para describir la estructura de una página web. / respuesta
+*/
+
+// Pregunta 8 - La etiqueta <html> indica el inicio del documento HTML. 
+// ¿Qué etiqueta debería aparecer al final del documento?
+
+/*
+    Opciones:
+
+    </html> / respuesta
+    <lmth>
+    <html>
+    <html/>
+*/
+
+// Pregunta 9 - ¿Qué etiqueta utilizamos en HTML para indicar la parte 
+// principal del documento dentro de la cual definimos los elementos de la página?
+
+/*
+    Opciones:
+
+    <head>
+    <main>
+    <content>
+    <body> / respuesta
+*/
+
+// Pregunta 10 - ¿Qué etiqueta HTML se utiliza para indicar que estamos incorporando código JS?
+
+/*
+    Opciones:
+
+    <source>
+    <js>
+    <execute>
+    <script> / respuesta
+*/
+
+// Pregunta 11 - En un navegador, queremos abrir un archivo local escribiendo su ruta 
+// en la barra de direcciones. La ruta debe ir precedida de:
+
+/*
+    Opciones:
+
+    file:/// / respuesta
+    http:// 
+    local:///
+    https://
+*/
+
+// Pregunta 12 - En el navegador, escribimos en la barra de direcciones una cadena que 
+// empieza por file:///. Esto significa que:
+
+/*
+    Opciones:
+
+    - No queremos abrir la página, sino solo descargar un archivo de la dirección 
+      remota y guardarlo en la máquina local.
+    - El resto de la cadena es la ruta al archivo en nuestra computadora local 
+      que queremos abrir en el navegador. / respuesta
+    - Queremos que el navegador cree una página vacía para nosotros con el nombre 
+      que le damos como continuación de la cadena.
+    - Queremos guardar la página como un archivo local.
+*/
+
+// Pregunta 13 - Ingresando about:blank en la barra de direcciones de su navegador:
+
+/*
+    Opciones:
+
+    - Restablece el navegador a su configuración predeterminada.
+    - Abre una pestaña con información sobre tu navegador.
+    - Genera una página con información sobre el estado del navegador y la envía al desarrollador.
+    - Genera y carga una página HTML mínima en blanco en la pestaña actual. / respuesta
+*/
+
+// Pregunta 14 - El código JS incluye el comando console.log("http://test.org");. Su ejecución:
+
+/*
+    Opciones:
+
+    - Envía un registro con información sobre el script actualmente ejecutado a la dirección 
+      indicada http://test.org.
+    - Muestra el siguiente mensaje en la consola: "http://test.org". / respuesta
+    - Muestra en la consola información sobre el progreso de carga de la página http://test.org.
+    - Hace que la página http://test.org se cargue en el navegador.
+*/
